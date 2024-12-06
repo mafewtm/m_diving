3a3c744bca6b71a2a9e77ecf3d5ff57e116f2656
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-12-06 18:44:18 -0500

    feat: manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'mafewtm'
+name 'm_diving'
+description 'Just another diving resource'
+repository 'https://github.com/mafewtm/m_diving'
+version '1.0.0'
+
+shared_script '@ox_lib/init.lua'
+
+client_script 'client/main.lua'
+
+server_script 'server/main.lua'
+
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
