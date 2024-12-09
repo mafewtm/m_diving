732bb79c98e59579c920140b81072c5b3c5f16be
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-12-08 23:33:57 -0500

    feat(config): wreck coords

---
+++ config/shared.lua
@@ -0,0 +1,60 @@
+return {
+    wrecks = {
+        {
+            coords = vec3(-2834.89, -469.6, -0.22), -- Decompression Chamber | Pacific Bluffs | 100 ft
+            points = {},
+        },
+        -- {
+        --     coords = vec3(-3175.94, 3031.2, -0.29), -- Tugboat | Fort Zancudo | 110 ft
+        --     points = {},
+        -- },
+        -- {
+        --     coords = vec3(-3258.95, 3674.29, 0.12), -- Cargo Plane Fort Zancudo | 70 - 200 ft
+        --     points = {},
+        -- },
+        -- {
+        --     coords = vec3(-906.4, 6657.24, -0.13), -- FlyUS Jet | Paleto Bay | 55 ft
+        --     points = {},
+        -- },
+        -- {
+        --     coords = vec3(2671.15, 6644.81, 0.06), -- Submarine | Mount Gordo | 50 ft
+        --     points = {},
+        -- },
+        -- {
+        --     coords = vec3(3269.64, 6413.06, 0.39), -- Duster | Mount Gordo | 130 ft
+        --     points = {},
+        -- },
+        -- {
+        --     coords = vec3(3399.58, 6313.68, -0.14), -- Tugboat | Mount Gordo | 140 ft
+        --     points = {},
+        -- },
+        -- {
+        --     coords = vec3(4177.41, 3580.49, -0.22), -- Military Debris Field | Humane Labs | 100 ft
+        --     points = {},
+        -- },
+        -- {
+        --     coords = vec3(3888.76, 3038.46, 0.01), -- Tugboar | Davis Quartz / Humane Labs | 80 ft
+        --     points = {},
+        -- },
+        -- {
+        --     coords = vec3(3173.46, -338.46, 0.2), -- Container Ship | NOOSE HQ | 20 ft
+        --     points = {},
+        -- },
+        -- {
+        --     coords = vec3(2675.99, -1393.34, 0.08), -- Steam Ship | Palamino | 40 ft
+        --     points = {},
+        -- },
+        -- {
+        --     coords = vec3(1822.74, -2925.48, -0.83), -- Jet | El Burro Heights | 65 ft
+        --     points = {},
+        -- },
+        -- {
+        --     coords = vec3(-209.64, -2866.32, -0.14), -- Tanker Ship | Port of LS | 20 ft
+        --     points = {},
+        -- },
+        -- {
+        --     coords = vec3(270.1, -2278.95, -0.34), -- Barge | Banning | 6 ft
+        --     points = {},
+        -- },
+    },
+}
