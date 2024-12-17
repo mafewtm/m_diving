81ec5ad614b9f28c6b9ba401826cc374dba60f9a
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-12-17 18:09:00 -0500

    fix(manifest): glob

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,9 +11,9 @@ ox_lib 'locale'
 
 shared_script '@ox_lib/init.lua'
 
-client_script 'client/main.lua'
+client_script 'client/*.lua'
 
-server_script 'server/main.lua'
+server_script 'server/*.lua'
 
 files {
     'config/client.lua',
