b175cd18d9c289f0bbeba1312e59047cb4e1017f
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-12-17 18:06:41 -0500

    feat: seperate gear

---
+++ client/gear.lua
@@ -0,0 +1,48 @@
+local config = require 'config.client'
+
+local function attachTank()
+    local netId = lib.callback.await('m_diving:server:spawnTank', false)
+    local tank = NetworkGetEntityFromNetworkId(netId)
+
+    if DoesEntityExist(tank) then
+        local bone = GetPedBoneIndex(cache.ped, 24818)
+
+        AttachEntityToEntity(tank, cache.ped, bone, -0.25, -0.25, 0.0, 180.0, 90.0, 0.0, true, true, false, false, 2, true)
+    end
+end
+
+local function useDivingGear()
+    if IsEntityDead(cache.ped) or IsPedSwimming(cache.ped) or cache.vehicle then
+        exports.qbx_core:Notify(locale('cannot_right_now'), 'error')
+        return
+    end
+
+    local alreadyUsingScuba = GetPedConfigFlag(cache.ped, 135, true)
+
+    if lib.progressBar({
+        duration = config.suitUseTime,
+        label = alreadyUsingScuba and locale('remove_gear') or locale('put_on_gear'),
+        useWhileDead = false,
+        allowSwimming = false,
+        anim = {
+            dict = 'clothingshirt',
+            clip = 'try_shirt_positive_d',
+            blendIn = 8.0
+        }
+    }) then
+        SetEnableScuba(cache.ped, not alreadyUsingScuba)
+
+        if alreadyUsingScuba then
+            TriggerServerEvent('m_diving:server:deleteTank')
+            ClearPedScubaGearVariation(cache.ped)
+        else
+            attachTank()
+            SetPedScubaGearVariation(cache.ped)
+        end
+
+        SetPedMaxTimeUnderwater(cache.ped, alreadyUsingScuba and 10.0 or 2000.0)
+    else
+        exports.qbx_core:Notify(locale('canceled'), 'error')
+    end
+end
+exports('UseDivingGear', useDivingGear)
--- server/main.lua
+++ server/main.lua
@@ -4,21 +4,7 @@ assert(lib.checkDependency('ox_inventory', '2.43.3'), 'ox_inventory v2.43.3 or h
 
 local config = require 'config.server'
 local sharedConfig = require 'config.shared'
-local playerTanks = {}
 local wreck = {}
-
----@param source number
-local function deleteTank(source)
-    local tank = playerTanks[source]
-
-    if tank then
-        if DoesEntityExist(tank) then
-            DeleteEntity(tank)
-        end
-
-        playerTanks[source] = nil
-    end
-end
 
 local function getNewLocation()
     wreck = {}
@@ -37,50 +23,10 @@ lib.callback.register('m_diving:server:getLocation', function()
    return wreck
 end)
 
-lib.callback.register('m_diving:server:spawnTank', function(source)
-    if playerTanks[source] then
-        deleteTank(source)
-    end
-
-    local tank = CreateObject(`p_s_scuba_tank_s`, 1.0, 1.0, 1.0, true, false, false)
-
-    lib.waitFor(function()
-        if DoesEntityExist(tank) then
-            return true
-        end
-    end, locale('failed_spawn'), 2000)
-
-    playerTanks[source] = tank
-
-    local netId = NetworkGetNetworkIdFromEntity(tank)
-
-    return netId
-end)
-
-AddEventHandler('playerDropped', function()
-    local src = source
-
-    deleteTank(src)
-end)
-
 AddEventHandler('onResourceStart', function(resource)
     if resource ~= cache.resource then return end
 
     getNewLocation()
-end)
-
-AddEventHandler('onResourceStop', function(resource)
-    if resource ~= cache.resource then return end
-
-    for i in pairs(playerTanks) do
-        deleteTank(i)
-    end
-end)
-
-RegisterNetEvent('m_diving:server:deleteTank', function()
-    local src = source
-
-    deleteTank(src)
 end)
 
 RegisterNetEvent('m_diving:server:lootCollected', function(source, index)
